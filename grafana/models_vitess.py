97c9d45e7acd9c1a372dd6196614c5d265b5e8c5
Mike Showerman <mshow@ncsa.illinois.edu>
2021-11-12 00:34:23 -0600

    Update models_vitess.py

--- grafana/models_vitess.py
+++ grafana/models_vitess.py
@@ -413,9 +413,9 @@ class Query(object):
                 if not str(params) :
 #                    query= "select "+str(metric)+",((Ctime DIV 60)*60) as minutex from "+str(metric_table)+" where CompId in (" + str(comp_id) + ") and cTime>"+str(start)+" and cTime<"+str(end) + " group by minutex"
                     if allnodes==0:
-                        query= "select "+str(metric)+",cTime from "+str(metric_table)+" where CompId in (" + str(comp_id) + ") and cTime>"+str(start)+" and cTime<"+str(end)
+                        query= "select "+str(metric)+",cTime from "+str(metric_table)+" where CompId in (" + str(comp_id) + ") and cTime>"+str(start)+" and cTime<"+str(end) + " group by cTime"
                     else:
-                        query= "select "+str(metric)+",cTime from "+str(metric_table)+" where cTime>"+str(start)+" and cTime<"+str(end) + " group by cTime"
+                        query= "select "+str(metric)+",cTime from "+str(metric_table)+" where cTime>"+str(start)+" and cTime<"+str(end)
                 else:
                     if allnodes==0:
                         query= "select "+str(params)+"("+str(metric)+"),cTime from "+str(metric_table)+" where CompId in (" + nidlist + ") and cTime>"+str(jobStart)+" and cTime<"+str(jobEnd) + " group by cTime"
